001156909ffec0559d1a2088f8805bda00d83050
Tanjim <ahmedtanjim336@gmail.com>
2026-01-23 03:52:39 +0600

    6th commit

--- src/assets/Auctions/Auction.jsx
+++ src/assets/Auctions/Auction.jsx
@@ -12,14 +12,14 @@ const Auction = ({ auctions ,handleFavouriteItem,handleremoveFavouriteItem,favou
                 return (
 
                          
-                    <div className={` text-2xl font-medium flex gap-20 items-center   p-10   bg-white border-l-2 border-r-2 border-b-2 border-black ${isLast ? "rounded-bl-2xl rounded-br-2xl " : ""}`} key={auction.id}>
+                    <div className={` text-2xl font-medium flex gap-20 items-center   p-6   bg-white border-l-2 border-r-2 border-b-2 border-black ${isLast ? "rounded-bl-2xl rounded-br-2xl " : ""}`} key={auction.id}>
 
-                   <div className="flex gap-5 items-center w-[45%]">
-                 <img className="w-22 h-20" src={auction.image} alt="none" />
+                   <div className="flex gap-5 items-center w-[45%] ">
+                 <img className="w-22 h-20 mr-4" src={auction.image} alt="none" />
                <h3>{auction.description}</h3>
                     </div>
 
-                    <div className="flex gap-5 items-center w-[55%] justify-between mr-20">
+                    <div className="flex gap-5 items-center w-[55%] justify-between mr-10">
                     <h4>${auction.currentBidPrice}</h4>
                     <h2>{auction.timeLeft}</h2>
                     <Button handleFavouriteItem={handleFavouriteItem}  item={auction} handleremoveFavouriteItem={handleremoveFavouriteItem} favourites={favourites}></Button>
